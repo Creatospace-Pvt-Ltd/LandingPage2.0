ad058481f82edad0e26a5068b40b6dd6d6ef3c29
Aman Kr. Singh <aman986838@gmail.com>
2023-07-24 23:11:41 +0530

    .

--- src/Components/Countdown.tsx
+++ src/Components/Countdown.tsx
@@ -43,7 +43,7 @@ const CountdownTimer = () => {
   }, []);
 
   return (
-    <div className="w-full h-[80px] flex justify-center items-center bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 my-8 py-5">
+    <div className="w-full h-[80px] max-md:h-max flex justify-center items-center bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 my-8 py-5">
       {timeRemaining.days === 0 &&
       timeRemaining.hours === 0 &&
       timeRemaining.minutes === 0 &&
@@ -51,21 +51,21 @@ const CountdownTimer = () => {
         <div>Countdown Expired</div>
       ) : (
         <div className="w-full flex font-monster text-md justify-center  flex-col items-center h-max gap-2">
-          <p className="text-xl">{`Create your Community and get free Premium Access`}</p>
+          <p className="text-xl max-md:text-sm text-center">{`Create your Community and get free Premium Access`}</p>
           <div>
-            <span className="p-1 bg-black px-7 rounded-md text-2xl">
+            <span className="p-1 bg-black px-7 rounded-md text-2xl max-md:text-md max-md:px-2">
               {timeRemaining.days}
             </span>{" "}
             days{" "}
-            <span className="p-1 bg-black px-7 rounded-md text-2xl">
+            <span className="p-1 bg-black px-7 rounded-md text-2xl max-md:text-md max-md:px-2">
               {timeRemaining.hours}
             </span>{" "}
             hrs{" "}
-            <span className="p-1 bg-black px-7 rounded-md text-2xl">
+            <span className="p-1 bg-black px-7 rounded-md text-2xl max-md:text-md max-md:px-2">
               {timeRemaining.minutes}
             </span>{" "}
             min{" "}
-            <span className="p-1 bg-black px-7 rounded-md text-2xl">
+            <span className="p-1 bg-black px-7 rounded-md text-2xl max-md:text-md max-md:px-2">
               {timeRemaining.seconds}
             </span>{" "}
             sec
--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,40 +1,65 @@
 import gif from "../assets/anim.gif";
 import "../index.css";
-import { BsWindows, BsAndroid2 } from "react-icons/bs";
+import { BsWindows } from "react-icons/bs";
 const HeroSection = () => {
   return (
-    <div className="h-max w-[95%] my-20 mx-auto flex pt-[90px]">
-      <div className="w-[80%] flex justify-center items-start flex-col pl-10 gap-4">
-        <div className=" w-[90%] flex flex-col gap-5 font-monster">
-          <p className="text-[50px] tracking-tighter font-monster font-[700] text-balance leading-[60px]">
-            Create the Most{" "}
-            <span className="text-yellow-500">
-              Immersive Community Experience
-            </span>{" "}
-            for your Audience with the Power of
-            <span className="text-yellow-500"> Metaverse</span>
-          </p>
-          <p>
-            Give your audience webinars & workshops, conduct meetups as well as
-            stream your content in virtual spaces and make a wholesome and
-            personalized experience for your members
-          </p>
+    <section className="flex flex-col items-center">
+      <div className="h-max w-[95%] my-20 mx-auto flex pt-[90px]">
+        <div className="w-[80%] flex justify-center items-start flex-col pl-10 gap-4">
+          <div className=" w-[90%] flex flex-col gap-5 font-monster">
+            <p className="text-[50px] max-md:text-base tracking-tighter font-monster font-[700] text-balance leading-[60px]">
+              Create the Most{" "}
+              <span className="text-yellow-500">
+                Immersive Community Experience
+              </span>{" "}
+              for your Audience with the Power of
+              <span className="text-yellow-500"> Metaverse</span>
+            </p>
+            <p className="max-md:text-xs">
+              Give your audience webinars & workshops, conduct meetups as well
+              as stream your content in virtual spaces and make a wholesome and
+              personalized experience for your members
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 max-sm:hidden">
+            <a
+              href="https://link.creatospace.com/windownload"
+              className="w-[300px] h-[55px] max-md:w-[250px ] flex justify-between items-center rounded-md bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600  p-2 cursor-pointer"
+            >
+              <p className="w-[80%] max-md:text-sm max-md:w-[90%]">
+                Download <span className="font-bold">Windows(x64)</span>{" "}
+              </p>
+              <p className="max-md:text-md">
+                <BsWindows />
+              </p>
+            </a>
+          </div>
         </div>
-        <div className="flex flex-col gap-3">
-          <a
-            href="https://link.creatospace.com/windownload"
-            className="w-[300px] h-[55px] flex justify-between items-center rounded-md bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600  p-2 cursor-pointer"
-          >
-            {" "}
-            <p className="w-[80%]">
-              Download <span className="font-bold">Windows(x64)</span>{" "}
-              Application
-            </p>
-            <p className="text-[24px]">
-              <BsWindows />
-            </p>
-          </a>
-          {/* <div className="w-[300px] h-[55px] flex justify-between items-center rounded-md gradientBorder p-2 cursor-pointer">
+        <div className="flex justify-center items-center flex-grow">
+          <img src={gif} alt="" className="h-[85%]" />
+        </div>
+      </div>
+      <div className="hidden flex-col gap-2 max-sm:flex">
+        <a
+          href="https://link.creatospace.com/windownload"
+          className="w-[300px] h-[55px] max-md:w-[250px ] flex justify-between items-center rounded-md bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600  p-2 cursor-pointer"
+        >
+          <p className="w-[80%] max-md:text-sm max-md:w-[90%]">
+            Download <span className="font-bold">Windows(x64)</span> Application
+          </p>
+          <p className="max-md:text-md">
+            <BsWindows />
+          </p>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
+
+{
+  /* <div className="w-[300px] h-[55px] flex justify-between items-center rounded-md gradientBorder p-2 cursor-pointer">
             {" "}
             <p className="w-[80%]">
               Download <span className="font-bold">Android</span> Application
@@ -42,14 +67,5 @@ const HeroSection = () => {
             <p className="text-[24px]">
               <BsAndroid2 />
             </p>
-          </div> */}
-        </div>
-      </div>
-      <div className="flex justify-center items-center flex-grow">
-        <img src={gif} alt="" className="h-[85%]" />
-      </div>
-    </div>
-  );
-};
-
-export default HeroSection;
+          </div> */
+}
